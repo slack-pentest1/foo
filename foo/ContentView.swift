d19469bca421ceef34572cdadb267ffafeb5ad83
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-08-21 18:59:19 +0530

    Initial Commit

---
+++ foo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  foo
+//
+//  Created by Test Ios on 21/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ foo/fooApp.swift
@@ -0,0 +1,17 @@
+//
+//  fooApp.swift
+//  foo
+//
+//  Created by Test Ios on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct fooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
